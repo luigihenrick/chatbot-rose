eec9b23ee4f36b4ce1d97e1152fa29204065255a
Luigi Henrick <luigihenrick@gmail.com>
2019-02-10 21:40:01 -0200

    Adicionado porta as variaveis de ambiente

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.json());
 
