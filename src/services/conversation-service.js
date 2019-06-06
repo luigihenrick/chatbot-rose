353ae42eda0f4a489cd4bba29802825d6a9ff88e
Luigi Henrick <luigihenrick@gmail.com>
2019-06-06 20:48:26 -0300

    Added login by existing user

--- src/services/conversation-service.js
+++ src/services/conversation-service.js
@@ -1,15 +1,15 @@
 var UserModel = require('../models/user');
 var ConversationModel = require('../models/conversation');
 
-async function saveConversation(params){
+async function saveConversation(params) {
     const user = await UserModel.findOne({ phonenumber: params.context.telefone.replace(new RegExp(phoneReplaceRegex, 'g'), '') });
     let model = await ConversationModel.findOne({ conversation_id: params.context.conversation_id });
-    
+
     if (user && model) {
         model.user_mood = params.context.estadoUsuario;
         model.last_node_visited = params.output.nodes_visited[params.output.nodes_visited.length - 1];
 
-        await ConversationModel.updateOne({conversation_id: params.context.conversation_id}, model);
+        await ConversationModel.updateOne({ conversation_id: params.context.conversation_id }, model);
     } else if (user) {
         model = new ConversationModel();
 
@@ -17,11 +17,26 @@ async function saveConversation(params){
         model.user_mood = params.context.estadoUsuario;
         model.conversation_id = params.context.conversation_id;
         model.last_node_visited = params.output.nodes_visited[params.output.nodes_visited.length - 1];
-    
+
         await model.save();
     }
 
     return model;
 }
 
-module.exports = { saveConversation }
+function getConversationModel(params) {
+    let model = new ConversationModel();
+
+    if (params.context) {
+        model.user_mood = params.context.estadoUsuario;
+        model.conversation_id = params.context.conversation_id;
+    }
+
+    if (params.output){
+        model.last_node_visited = params.output.nodes_visited[params.output.nodes_visited.length - 1];
+    }
+    
+    return model;
+}
+
+module.exports = { saveConversation, getConversationModel }
